e35ec3c5b095427b49ad24d76c1fc0f52563bf6a
Mansur Ziiatdinov <gltronred@gmail.com>
2016-02-26 01:26:19 +0300

    Example 2

--- src/Main.hs
+++ src/Main.hs
@@ -10,8 +10,16 @@ ex1 = do
   sendPast 10
   return $ show a ++ " " ++ show b
 
+ex2 :: Tardis [Int] () [Int]
+ex2 = do
+  fibs <- getFuture
+  modifyBackwards $ scanl (+) 0
+  sendPast $ 1:fibs
+  return fibs
+
 main :: IO ()
 main = do
   putStrLn "hello world"
   print $ runTardis ex1 (1,2.0)
+  print $ take 10 $ evalTardis ex2 ([],())
 
